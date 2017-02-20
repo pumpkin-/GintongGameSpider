a68957f7b3e28ed38f77f015b5c76a40ec2cc265
sunjianan <897692892@qq.com>
2017-02-20 14:09:17 +0800

    第二次提交

--- src/SpiderUtils/TestDebug.java
+++ src/SpiderUtils/TestDebug.java
@@ -4,4 +4,17 @@ package SpiderUtils;
  * Created by Administrator on 2017/2/18.
  */
 public class TestDebug {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        System.out.print(i);
+
+        System.out.print(i);
+
+
+
+    }
+
 }
