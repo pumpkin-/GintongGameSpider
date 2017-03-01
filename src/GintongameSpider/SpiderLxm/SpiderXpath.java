a38d778c7102761a3a9a266c82fa5dee1bd18bce
18811737901 <18811737901@163.com>
2017-03-01 10:30:26 +0800

    刘雪明提交

--- src/GintongameSpider/SpiderLxm/SpiderXpath.java
+++ src/GintongameSpider/SpiderLxm/SpiderXpath.java
@@ -17,7 +17,7 @@ public class SpiderXpath {
             @Override
             public void run() {
                 try {
-                    CommonSpiderKnowledge.ergodicUrl("spiderGameKZ",0,"no");
+                    CommonSpiderKnowledge.ergodicUrl("spiderVR",0,"no");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
