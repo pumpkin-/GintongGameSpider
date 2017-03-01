64fd8ac1307c3ab4614e04e14b1af464f98ac9cd
ligang <15711490906@163.com>
2017-03-01 20:50:51 +0800

    李罡

--- src/SpiderUtils/SpiderGameData.java
+++ src/SpiderUtils/SpiderGameData.java
@@ -16,11 +16,34 @@ public class SpiderGameData {
     public static void main(String [] args){
         try {
 
-           //CommonSpiderKnowledge.ergodicUrl("spider52PKRW", 0, "no");
-          // CommonSpiderKnowledge.ergodicUrl("spider52PKCY", 0, "no");
-          // CommonSpiderKnowledge.ergodicUrl("spider52PKDL", 0, "no");
-           // CommonSpiderKnowledge.ergodicUrl("spider52PKQQ", 0, "no");
-            CommonSpiderKnowledge.ergodicUrl("spiderYXC", 0, "no");
+             //跑完
+          //  CommonSpiderKnowledge.ergodicUrl("spider52PKRW", 0, "no");
+//
+//           CommonSpiderKnowledge.ergodicUrl("spider52PKQQ", 0, "no");
+//            Thread t1=new Thread(){
+//                @Override
+//                public void run() {
+//                    try {
+                          //有null 需要模版｜｜
+//                        CommonSpiderKnowledge.ergodicUrl("spider52PKDL", 0, "no");
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            };
+          Thread t2=new Thread(){
+               @Override
+               public void run() {
+                    try {
+                        CommonSpiderKnowledge.ergodicUrl("spider52PKCY", 158, "no");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                   }
+                }
+            };
+//           t1.start();
+            t2.start();
+           // CommonSpiderKnowledge.ergodicUrl("spiderYXC", 0, "no");
         } catch (Exception e) {
             e.printStackTrace();
         }
