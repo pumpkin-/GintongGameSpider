a91d4f3a336790faab6645c5df3f0f6930c5ec33
dingquanbin <dingquanbin66@163.com>
2017-03-01 15:00:36 +0800

    拉取最新代码

--- src/SpiderUtils/CommonSpiderKnowledge.java
+++ src/SpiderUtils/CommonSpiderKnowledge.java
@@ -673,7 +673,7 @@ public class CommonSpiderKnowledge {
         //作者
         if(StringUtils.isNotEmpty(knowledgeSpiderConfig.author.getText())) {
             if (map.get("author")==null||map.get("author").size()<=1) {
-                String test[] = getTagOne(childDocumet,knowledgeSpiderConfig.author.getText()).toString().split("\\s+");
+                String test[] = getTagOne(childDocumet,knowledgeSpiderConfig.author.getText()).toString().split(" +");
                 if(test.length>1) {
                     for (int y = 0; y < test.length; y++) {
                         Pattern pat = Pattern.compile(".*作者.*");
