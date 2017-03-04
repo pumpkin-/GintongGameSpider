018f4565582fea922b58c0851cdc1aa1bb3192c5
18811737901 <18811737901@163.com>
2017-03-04 09:12:00 +0800

    刘雪明提交

--- src/SpiderUtils/SpiderContant.java
+++ src/SpiderUtils/SpiderContant.java
@@ -13,10 +13,8 @@ import java.util.List;
  * Created by lenovo on 2017/2/11.
  */
 public class SpiderContant {
-
     //持久化存储批量插入条数
     public static final int insertBatchContant = 5;
-    public static final String phantomjsLinuxPath="/Spider/phantomjs-2.1.1-linux-x86_64/bin/phantomjs";
+    public static final String phantomjsLinuxPath="D:/Spider/phantomjs-2.1.1-linux-x86_64/bin/phantomjs.exe";
     public static final String chromeWindowsPath= "E:/aaaa/chrom/chromedriver.exe";
-
 }
