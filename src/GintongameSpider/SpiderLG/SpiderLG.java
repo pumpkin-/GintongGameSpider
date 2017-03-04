c563bb664ebc3800aa4fd16e6a6dbcbe2b5fa5c9
jhy6677 <925611923@qq.com>
2017-03-04 15:41:46 +0800

    贾宏宇

--- src/GintongameSpider/SpiderLG/SpiderLG.java
+++ src/GintongameSpider/SpiderLG/SpiderLG.java
@@ -1,14 +1,12 @@
 package GintongameSpider.SpiderLG;
 
 import SpiderUtils.CommonSpiderKnowledge;
-import SpiderUtils.SpiderProduct;
 
 /**
  * Created by lenovo on 2017/2/28.
  */
 public class SpiderLG {
     public static void main(String [] args) throws Exception {
-       SpiderProduct.ergodicUrl("SpiderYxg", 0);
-        //SpiderProduct.ergodicUrl("Spider4399", 0);
+        CommonSpiderKnowledge.ergodicUrl("spiderYzwTwo", 8, "no");
     }
 }
