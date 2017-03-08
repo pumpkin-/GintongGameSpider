bee15091cc5e0fb1e3a1faf1a0ef204d1a7e1fe9
dingquanbin <dingquanbin66@163.com>
2017-03-08 10:23:18 +0800

    提交产品代码

--- src/GintongameSpider/SpiderLG/SpiderLG.java
+++ src/GintongameSpider/SpiderLG/SpiderLG.java
@@ -8,11 +8,7 @@ import SpiderUtils.SpiderProduct;
  */
 public class SpiderLG {
     public static void main(String [] args) throws Exception {
-<<<<<<< HEAD
        SpiderProduct.ergodicUrl("SpiderYxg", 0, 0);
-=======
-       SpiderProduct.ergodicUrl("SpiderYxg", 0, 1);
->>>>>>> 8d487ab1bc8094a9dd996be3cb3a0f30ce49e47a
 
     }
 }
